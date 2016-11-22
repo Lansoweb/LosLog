4adec4cbe93b20d36c134f5cd6ce481cbe915d85
Leandro Silva <leandro.silva@zoox.com.br>
2016-11-22 16:06:15 -0200

    Adding more details to the error output

--- src/LosLog.php
+++ src/LosLog.php
@@ -18,8 +18,8 @@ class LosLog implements ErrorMiddlewareInterface
 
     public function __invoke($error, Request $request, Response $response, callable $next = null)
     {
-        if ($error instanceof \Exception) {
-            $this->logger->error($error->getMessage());
+        if ($error instanceof \Throwable) {
+            $this->logger->error($error->getMessage() . '. File: ' . $error->getFile() . ':' . $error->getLine());
         }
 
         if ($next !== null) {
