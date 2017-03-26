73969c4a55fd8032a45c3f6f31484e5f2ae07a33
Leandro Silva <leandro.silva@zoox.com.br>
2017-03-26 10:33:40 -0300

    Using the LoggerInterface

--- src/ErrorHandlerListenerDelegatorFactory.php
+++ src/ErrorHandlerListenerDelegatorFactory.php
@@ -2,6 +2,7 @@
 namespace LosMiddleware\LosLog;
 
 use Interop\Container\ContainerInterface;
+use Psr\Log\LoggerInterface;
 use Zend\ServiceManager\Factory\DelegatorFactoryInterface;
 use Zend\Stratigility\Middleware\ErrorHandler;
 
@@ -13,7 +14,10 @@ class ErrorHandlerListenerDelegatorFactory implements DelegatorFactoryInterface
      */
     public function __invoke(ContainerInterface $container, $name, callable $callback, array $options = null)
     {
-        $logger = $container->get(LosLog::class);
+        if (!$container->has(LoggerInterface::class)) {
+            throw new \RuntimeException("You must define a factory for LoggerInterface. Check loslog.global.php.dist for an example.");
+        }
+        $logger = $container->get(LoggerInterface::class);
 
         /* @var ErrorHandler $errorHandler */
         $errorHandler = $callback;
